609f59b86687493191039f54d85f16c9640e0f6b
VivienRobotics <vtchengang@ukrobotics.net>
2025-06-10 09:20:18 +0100

    added test

--- dispense/controller.py
+++ dispense/controller.py
@@ -1,19 +1,22 @@
+# file: dispense/d2_controller.py
 
-import serial
-import time
-import json
 import clr
 import os
 import sys
 
-# Add DLL folder to sys.path
-DLL_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "dlls"))
-sys.path.append(DLL_DIR)
-
-# Load the assembly (no .dll extension)
+dll_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "dlls"))
+sys.path.append(dll_dir)
 clr.AddReference("UKRobotics.MotorControllerLib")
 
-
-from UKRobotics import MotorControllerLib
+from UKRobotics.MotorControllerLib import D2Controller as D2ControllerDotNet
 
 class D2Controller:
+    def __init__(self, port: str, baud: int = 115200):
+        self._ctrl = D2ControllerDotNet()
+        self._ctrl.OpenComms(port, baud)
+
+    def dispose(self):
+        self._ctrl.Dispose()
+
+    def move_z_axis(self, pos):
+        self._ctrl.ZAxis.MoveAbsolute(pos)
